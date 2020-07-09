93154552593bfdc3178cf54ae47e1baf794d06c1
zydrax23@gmail.com <zydrax23@gmail.com>
2020-07-09 18:47:24 +0300

    update  models

---
+++ ProjectWebApi/ProjectWebApi/Entities/Atractie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWebApi.Entities
+{
+    public class Atractie
+    {
+        [Key]
+
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+
+        public string Adresa { get; set; }
+        [Required]
+        [MaxLength(2500)]
+
+        public string Description { get; set; }
+
+
+        public int Pret { get; set; }
+
+        [ForeignKey("OrasId")]
+
+        public virtual Oras Oras { get; set; }
+
+
+        public bool? Deleted { get; set; }
+
+
+
+    }
+}
---
+++ ProjectWebApi/ProjectWebApi/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWebApi.Entities
+{
+    public class User
+    {
+        [Key]
+
+        public Guid Id { get; set; }
+        [MaxLength(150)]
+        public string FirstName { get; set; }
+        [MaxLength(150)]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public bool isAdmin { get; set; }
+
+        public bool? Deleted { get; set; }
+
+
+
+
+    }
+}
