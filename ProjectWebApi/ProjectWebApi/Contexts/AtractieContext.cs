4beabc02fec35d412e7030117007559fb069d160
zydrax23@gmail.com <zydrax23@gmail.com>
2020-07-16 15:08:46 +0300

    update context and migrations

---
+++ ProjectWebApi/ProjectWebApi/Contexts/AtractieContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectWebApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWebApi.Contexts
+{
+    public class AtractieContext : DbContext
+    {
+        public AtractieContext(DbContextOptions<AtractieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Atractie> Attractions { get; set; }
+
+        public DbSet<Cazare> Bookings { get; set; }
+
+        public DbSet<Tara> Countries { get; set; }
+
+        public DbSet<Oras> Cities { get; set; }
+
+
+            
+
+    }
+}
---
+++ ProjectWebApi/ProjectWebApi/Migrations/20200714150413_initialCreate.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectWebApi.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 150, nullable: true),
+                    Description = table.Column<string>(maxLength: 1500, nullable: true),
+                    Deleted = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 150, nullable: true),
+                    LastName = table.Column<string>(maxLength: 150, nullable: true),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    isAdmin = table.Column<bool>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 150, nullable: true),
+                    Description = table.Column<string>(maxLength: 1500, nullable: true),
+                    TaraId = table.Column<Guid>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_TaraId",
+                        column: x => x.TaraId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attractions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 150, nullable: false),
+                    Adresa = table.Column<string>(maxLength: 150, nullable: false),
+                    Description = table.Column<string>(maxLength: 2500, nullable: false),
+                    Pret = table.Column<int>(nullable: false),
+                    OrasId = table.Column<Guid>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attractions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attractions_Cities_OrasId",
+                        column: x => x.OrasId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 150, nullable: false),
+                    Adresa = table.Column<string>(maxLength: 150, nullable: false),
+                    Description = table.Column<string>(maxLength: 2500, nullable: false),
+                    Review = table.Column<string>(maxLength: 150, nullable: false),
+                    Pret = table.Column<int>(nullable: false),
+                    OrasId = table.Column<Guid>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Cities_OrasId",
+                        column: x => x.OrasId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attractions_OrasId",
+                table: "Attractions",
+                column: "OrasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_OrasId",
+                table: "Bookings",
+                column: "OrasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_TaraId",
+                table: "Cities",
+                column: "TaraId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attractions");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
